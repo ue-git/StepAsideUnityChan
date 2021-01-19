5458022342a81ab869f0447f74d1e1e66bd3091e
ue-git <67855463+ue-git@users.noreply.github.com>
2021-01-20 08:59:28 +0900

    通常課題作成

--- Assets/ItemDestory.cs
+++ Assets/ItemDestory.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyCameraController : MonoBehaviour
+public class ItemDestory : MonoBehaviour
 {
     //Unityちゃんのオブジェクト
     private GameObject unitychan;
-    //Unityちゃんとカメラの距離
+    //Unityちゃんと削除判定オブジェクトの距離
     private float difference;
 
     // Start is called before the first frame update
@@ -21,7 +21,19 @@ public class MyCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Unityちゃんの位置に合わせてカメラの位置を移動
+        //Unityちゃんの位置に合わせて削除判定オブジェクトを移動
         this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
     }
+
+    //トリガーモードでほかのオブジェクトと接触した場合の処理
+    void OnTriggerEnter(Collider other)
+    {
+        //障害物に衝突した場合（車・コーン・コイン）
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag"　|| other.gameObject.tag == "CoinTag")
+        {
+            //衝突判定されたobject削除
+            Destroy(other.gameObject);
+        }
+    }
+
 }
